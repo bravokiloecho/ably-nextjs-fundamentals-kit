7186f7a1e2a4200226ec7a9aab0bd91b2e38b1d8
Benjamin E <ben@elwyn.co>
2025-07-01 10:51:36 +0100

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
-      "manrope": "Manrope, sans-serif",
-      "jetbrains-mono": "'JetBrains Mono', monospace",
+      manrope: 'Manrope, sans-serif',
+      'jetbrains-mono': "'JetBrains Mono', monospace",
     },
     extend: {
       colors: {
-        'linkedin':'#1269BF',
-        'discord':'#5B64EA'
-      }
+        linkedin: '#1269BF',
+        discord: '#5B64EA',
+      },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
 }
